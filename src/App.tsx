52dc2afec3a9d45f56176041418cb661bb88801a
XLS <cogassien@hotmail.com>
2026-03-04 17:24:24 +0100

    fix router for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { AccessibilityPage } from './components/pages/Acessibility';
 // import { Header } from './components/ui'
 import { ThemeProvider } from './context/themeContext'
