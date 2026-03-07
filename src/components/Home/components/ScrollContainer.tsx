b7aa5e7d98a2c4c131d1d709d3e9615dd8e320ba
XLS <cogassien@hotmail.com>
2026-03-07 19:58:57 +0100

    fix: title place for section 2 (for phone test_6)

--- src/components/Home/components/ScrollContainer.tsx
+++ src/components/Home/components/ScrollContainer.tsx
@@ -8,7 +8,7 @@ export const ScrollContainer: React.FC<ScrollContainerProps> = ({
     <div
         style={{
             position: 'relative',
-            height: '100vh',
+            height: '100%',
             width: '100%',
             overflow: 'hidden',
             pointerEvents: 'none',
